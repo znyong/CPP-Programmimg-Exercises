1310e916ad06136871845f1aca7f7bef39e34c26
znyong <103489301+znyong@users.noreply.github.com>
2022-05-08 22:52:30 +0900

    Add files via upload

---
+++ MonsterWorldGame.cpp
@@ -0,0 +1,17 @@
+#include "MonsterWorld.h"
+#include <time.h>
+int main()
+{
+	srand((unsigned int)time(NULL));
+	int w = 16, h = 8;
+
+	MonsterWorld game(w, h);
+	Monster m("몬스터", "※", rand() % w, rand() % h);
+	game.add(m);
+	game.add(Monster("도깨비", "◎", rand() % w, rand() % h));
+	game.add(Monster("별그대", "★", rand() % w, rand() % h));
+	game.add(Monster("고스트", "♥", rand() % w, rand() % h));
+	game.play(500, 10);
+	printf("------게임 종료-------------------\n");
+	return 0;
+}
